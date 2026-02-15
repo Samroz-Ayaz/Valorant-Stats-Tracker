e1c486100745ca96ef986adbe59012f6ed50a3c1
Samroz Ayaz <samroz.ayaz@gmail.com>
2026-02-15 13:25:30 -0600

    Style cards, add background

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Card from './Card';
 import './index.css'
+import haven from './assets/havenn.jpg';
+
 function App() {
 	const [name, setName] = useState("roz");
 	const [tag, setTag] = useState("frag");
@@ -21,46 +23,45 @@ function App() {
 		setShowCard(false);
 		setLoadShowCard(true);
         const response = await fetch(`http://localhost:8000/rank/${name}/${tag}/${region}`);
-        
         if (!response.ok) {
-            // Instead of just throwing, handle the UI state here
             console.error("Fetch failed");
             setLoadShowCard(false);
             return; 
         }
-
         const result = await response.json();
         setData(result);
-        
-        // 2. Data is ready, swap loading for the result card
         setLoadShowCard(false);
         setShowCard(true);
 	}
   return (
-	<div className="flex text-center justify-center min-h-screen transition-all gap-0">
-		<div className="flex-col w-1/5 bg-background text-text font-bold text-xl p-8 my-auto shadow-[0_0_80px_1px_#111823] rounded-xl">
-			<p className="text-4xl mb-1">Username</p>
-			<input className="text-2xl border-2 border-white rounded-md" onChange={handleNameChange}/>
-			<p className="text-4xl mb-1 mt-2">Tag</p>
-			<input className="text-2xl border-2 border-white rounded-md" onChange={handleTagChange}/>
-			<p className="text-4xl mt-2 mb-2">Region</p>
-			<select className="text-2xl border-2 border-white rounded-md" onChange={handleRegionChange}>
-				<option value="">Select an Option</option>
-				<option value="na">North America</option>
-				<option value="eu">Europe</option>
-			</select>
-			<br></br>
-			<button className="bg-slate-700 px-10 hover:bg-slate-800 transition-all rounded mt-4" onClick={handleSubmit}>Get Rank</button>
-		</div>
-		<div className="my-auto w-1/5">
-				{showLoadCard && (
-				<Card rank="" peak="" peak_szn="" loading ={true} className=""/>
-				)}
-				{showCard && (
-					<Card rank={data.curr_rank} peak={data.peak} peak_szn={data.peak_szn} loading={false} className=""/>
-				)}
+	<div className="min-h-screen w-full relative bg-[url(./assets/havenn.jpg)] bg-no-repeat bg-size-[120%] bg-position-[20%_10]">
+		<div className="absolute inset-0 bg-black/40">
+			<div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#ffffff12_1px,transparent_1px),linear-gradient(to_bottom,#ffffff12_1px,transparent_1px)] bg-size-[24px_24px]" >
+				<div className="flex text-center justify-center min-h-screen transition-all gap-20">
+					<div className="flex-col border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] text-text font-bold text-xl p-8 my-auto shadow-[0_0_80px_10px_#111823] rounded-xl">
+						<p className="text-4xl mb-1">Username</p>
+						<input className="text-2xl border-2 border-white rounded-md" onChange={handleNameChange}/>
+						<p className="text-4xl mb-1 mt-2">Tag</p>
+						<input className="text-2xl border-2 border-white rounded-md" onChange={handleTagChange}/>
+						<p className="text-4xl mt-2 mb-2">Region</p>
+						<select className="text-2xl border-2 border-white rounded-md" onChange={handleRegionChange}>
+							<option value="">Select an Option</option>
+							<option value="na">North America</option>
+							<option value="eu">Europe</option>
+						</select>
+						<br></br>
+						<button className="bg-slate-700 px-10 hover:bg-slate-800 transition-all rounded mt-4" onClick={handleSubmit}>Get Rank</button>
+					</div>
+					{showLoadCard && (
+					<Card rank="" peak="" peak_szn="" loading ={true} className=""/>
+					)}
+					{showCard && (
+						<Card rank={data.curr_rank} peak={data.peak} peak_szn={data.peak_szn} loading={false} className=""/>
+					)}
+				</div>
 			</div>
-    </div>
+		</div>
+	</div>
   );
 }
 export default App;
--- react-app/src/Card.jsx
+++ react-app/src/Card.jsx
@@ -3,17 +3,17 @@ function Card({rank, peak, peak_szn, loading}) {
   return(
     <>
     {loading ? (
-      <div className="text-3xl">
+      <div className="text-3xl flex flex-col text-white max-w-md my-auto shadow-[0_0_80px_1px_#111823] border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] p-19 rounded-2xl">
         <h1>Loading...</h1>   
       </div>
     ):(
 
-      <div className="show flex flex-col text-white max-w-md my-auto shadow-[0_0_80px_1px_#111823] bg-background p-19 rounded-2xl">
-        <h1 className="text-2xl">Current Rank:</h1>
-        <p className="">{rank}</p>
-        <h1 className="text-2xl">Peak:</h1>
-        <p className=''>{peak}</p>
-        <h1 className="text-2xl">In:</h1>
+      <div className="flex flex-col text-white max-w-md my-auto shadow-[0_0_80px_1px_#111823] border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] p-19 rounded-2xl">
+        <h1 className="text-4xl text-text">Current Rank:</h1>
+        <p className="mb-4">{rank}</p>
+        <h1 className="text-4xl text-text">Peak:</h1>
+        <p className='mb-4'>{peak}</p>
+        <h1 className="text-4xl text-text">In:</h1>
         <p className="">{peak_szn}</p>
     </div>
     )}
