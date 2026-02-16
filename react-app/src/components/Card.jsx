c30c3bfaad3abfef9f769511a58761400b59cc36
Samroz Ayaz <samroz.ayaz@gmail.com>
2026-02-15 22:09:32 -0600

    Change Font, and Reduce rank image size

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -12,16 +12,16 @@ function Card({rank, peak, peak_szn, loading}) {
       <div className="flex flex-col text-white max-w-md my-auto shadow-[0_0_80px_1px_#111823] border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] p-5 rounded-2xl">
         <h1 className="text-4xl text-text mb-3">Current Rank:</h1>
         <div className="flex justify-center">
-          <img src={`../../public/ranks/${rank}.png`} className="w-1/3 my-auto mx-auto"></img>
+          <img src={`../../public/ranks/${rank}.png`} className="w-1/4 my-auto mx-auto"></img>
           <h1 className="my-auto mx-auto text-3xl">{rank}</h1>
         </div>
         <h1 className="text-4xl text-text mb-3">Peak:</h1>
         <div className="flex justify-center">
-          <img src={`../../public/ranks/${rank}.png`} className="w-1/3 my-auto mx-auto"></img>
+          <img src={`../../public/ranks/${peak}.png`} className="w-1/4 my-auto mx-auto"></img>
           <h1 className="my-auto mx-auto text-3xl">{peak}</h1>
         </div>
         <h1 className="text-4xl text-text mb-3">In:</h1>
-        <h1 className="">{peak_szn}</h1>
+        <h1 className="text-3xl">{peak_szn}</h1>
     </div>
     )}
     </>
