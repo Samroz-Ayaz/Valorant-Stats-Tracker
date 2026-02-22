09c852cdbcace2e953b74f5a80ce0a5a80852b21
Samroz Ayaz <samroz.ayaz@gmail.com>
2026-02-21 21:33:00 -0600

    Make the matchCard div scrollable

--- react-app/src/components/MatchCard.jsx
+++ react-app/src/components/MatchCard.jsx
@@ -4,8 +4,10 @@ function MatchCard( { matchData }){
         matchname[i] = matchData.matches[i].map;
     }
     return(
-        <div className="py-10 animate-fade-in-up text-white shadow-[0_0_80px_1px_#111823] border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] rounded-2xl">
-            {matchname.map((x) => <h1>{x}</h1>)}
+        <div className="overflow-hidden animate-fade-in-up text-white shadow-[0_0_80px_1px_#111823] border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] rounded-2xl">
+            <div className="pl-4 h-25 overflow-y-auto custom-scrollbar">
+                {matchname.map((x) => <h1 className="py-2">{x}</h1>)}
+            </div>
         </div>
     )
 }
