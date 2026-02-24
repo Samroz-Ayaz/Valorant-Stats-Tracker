809e453c406391c8d00dea9ab9f93b9419c4a2ce
Samroz Ayaz <samroz.ayaz@gmail.com>
2026-02-23 20:35:56 -0600

    Add styling to match card

--- react-app/src/components/MatchCard.jsx
+++ react-app/src/components/MatchCard.jsx
@@ -4,17 +4,27 @@ function MatchCard( { matchData }){
         <div className="overflow-hidden animate-fade-in-up text-white shadow-[0_0_80px_1px_#111823] border-background border bg-black/30 backdrop-brightness-80 backdrop-blur-[3px] rounded-2xl">
             <div className="flex flex-col pl-4 h-50 overflow-y-auto custom-scrollbar">  
                 {matchData.matches.map((game) => 
-                <div className="mt-4 mx-auto w-98/100 py-10 border-t-2 pr-5">
-                    <div className="flex justify-between">
-                        <h1 className="text-xl">
+                <div className="mb-5 mx-auto w-98/100 border-t-2 pr-5">
+                    <div className="text-text mt-3 flex justify-between border-b border-white border-dashed">
+                        <h1 className="text-2xl">
                             {game.map} -- {game.mode}
                         </h1> 
-                        <h2 className={`${game.you_won ? `text-green-400` : `text-red-500`}`}>
+                        <h2 className={`text-xl ${game.you_won ? `text-green-400` : `text-red-500`}`}>
                             {game.score == "None - None" ? `` : `${game.score}`}
                         </h2>
                     </div>
-                    <h1>KDA: {game.stats.kills}/{game.stats.deaths}/{game.stats.assists}</h1>
-                    {game.stats.hs_percent != 0 ? <h1>Headshot Percent: {game.stats.hs_percent}%</h1> : <></>}
+                    <div className="pl-5 text-white">
+                    <div className="mt-4 flex justify-between">
+                        <h1 className="text-xl">KD: {(game.stats.kills / game.stats.deaths).toFixed(2)}</h1> 
+                        <h1>Score: {(game.stats.score)}</h1>
+                    </div>
+                    <h1 className="text-xl">Kills: {game.stats.kills}</h1>
+                    <h1 className="text-xl">Deaths: {game.stats.deaths}</h1>
+                    <h1 className="text-xl">Assists: {game.stats.assists}</h1>
+                    {game.stats.hs_percent != 0 ? <h1 className="text-xl">Headshot Percent: {game.stats.hs_percent}%</h1> : <></>}  
+                    </div>
+                        
+                    
                 </div>)} 
             </div>
         </div>
