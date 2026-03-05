8d4f5bdc8eacbb3722e28ced3afab074b3be3880
Samroz Ayaz <samroz.ayaz@gmail.com>
2026-03-05 17:30:55 -0600

    Add head shot% and score to match info

--- react-app/src/components/Match.jsx
+++ react-app/src/components/Match.jsx
@@ -1,29 +1,36 @@
 function Match ({ setShowMatch, oneMatch }){
     return(
         <div className="p-5 overflow-hidden w-full h-full animate-fade-in-up text-white shadow-[0_0_80px_1px_#111823] border-background border  backdrop-brightness-80 backdrop-blur-[3px] rounded-2xl">
-            <img onClick={() => setShowMatch(false)} src="../../x.webp" alt="X" className="absolute right-5 w-1/56"></img>
-            
-            <div className="flex items-center">
-                <img src={`../../Agents/${oneMatch.character}_icon.webp`} alt={oneMatch.character} className="w-1/11 ml-1 mr-5"></img>
-                <h1>{oneMatch.mode}</h1>
-                <div className="absolute right-1/5">
-                    <h1>Kills: {oneMatch.stats.kills}</h1>
-                    <h1>Deaths: {oneMatch.stats.deaths}</h1>
-                    <h1>Assists: {oneMatch.stats.assists}</h1>
+                <div className="absolute inset-4 flex justify-center">
+                    <h1 className={`text-2xl + ${oneMatch.you_won ? "text-green-400" : "text-red-500"}`}>{oneMatch.you_won ? "Victory" : "Loss"}</h1>
                 </div>
+                <img onClick={() => setShowMatch(false)} src="../../x.webp" alt="X" className="absolute right-5 w-1/56"></img>    
+
+            
+            <div className="flex items-center pb-4 mb-4 border-b-2 border-text">
+                <img src={`../../Agents/${oneMatch.character}_icon.webp`} alt={oneMatch.character} className="w-1/11 ml-1 mr-5"></img>
+                <div>
+                    <h1 className="text-2xl">{oneMatch.mode}</h1>
+                    <h1>KDA: {oneMatch.stats.kills}/{oneMatch.stats.deaths}/{oneMatch.stats.assists}</h1>
+                   
+                </div>
+                {oneMatch.mode != "Deathmatch" ? 
+                    <div className="flex-col absolute right-1/10 justify-between text-right items-center">
+                        <h1 className="text-1xl right-1">Headshot Percent: {oneMatch.stats.hs_percent}%</h1>
+                        <h1 className="text-1xl right-1">Score: {oneMatch.stats.score}</h1>
+                    </div> :
+                    <></>}
             </div>
-            <div className="flex justify-between">
-                {oneMatch.round_wins.map((data , x) =>(
+            <div className="">
+                <div className="flex justify-between px-2">
+                {oneMatch.mode != "Deathmatch" ? oneMatch.round_wins.map((data , x) =>(
                     <div className="flex-col justify-center">
                         <h1 className="flex justify-center">Round: {x}</h1>
-                        <h1 className="flex justify-center"> {data == oneMatch.team ? "win" : "loss"}</h1>
+                        <h1 className="flex justify-center"> {data == oneMatch.team ? "Win" : "Loss"}</h1>
                     </div>
-                    ))}
-            </div>
-            
-
-                    
-            
+                    )) : <></>}
+                </div>
+            </div>  
         </div>
     )
 }
